3af5d29548250ab99380d173694da90d8f32c0c3
SilverStardust <Yuristeelf@gmail.com>
2022-10-26 10:30:33 +0200

    upgrade

--- Main.py
+++ Main.py
@@ -1,20 +1,26 @@
 import discord
+import sys
 
-intents = discord.Intents.default()
-intents.message_content = True
+def start(token: str):
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-client = discord.Client(intents=intents)
+    client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user: return
+    @client.event
+    async def on_message(message):
+        if message.author == client.user: return
 
-    if message.content.startswith('$hello'): await message.channel.send('Hello!')
+        if message.content.startswith('$hello'): await message.channel.send('Hello!')
     
-    if message.content.startswith('$Shifumi'): await message.channel.send(f'Implementation for {str(message.author).split("#")[1]}')
+        if message.content.startswith('$Shifumi'): await message.channel.send(f'Implementation for {str(message.author).split("#")[1]}')
 
-client.run('MTAzNDA4NjY5ODA3NTgxNTk1Ng.GSSRLK.Ainw04FbAxv5LbIvbmj3VQEcmgXfB6zQnlkoEg')
+    client.run(token)
+    
+if __name__ == '__main__':
+    eval(sys.argv[1])
+    
